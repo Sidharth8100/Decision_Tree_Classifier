c28529f6461eb16a8b5d442f4cecc2fcf7b96a6b
Sidharth Bhagat <147747690+Sidharth8100@users.noreply.github.com>
2024-11-07 15:22:54 +0530

    Create script.js
    Part of gui please don't change anything added only for reference

---
+++ script.js
@@ -0,0 +1,29 @@
+function uploadFile() {
+    const fileName = document.getElementById('fileName').value;
+    
+    fetch('/upload', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ fileName })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('treeOutput').innerText = data.output;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function predict() {
+    const testFileName = document.getElementById('testFileName').value;
+
+    fetch('/predict', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ testFileName })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('predictionOutput').innerText = data.result;
+    })
+    .catch(error => console.error('Error:', error));
+}
